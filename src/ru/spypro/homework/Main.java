3233ea632fe50caae5cbfe5e9976db66d997384f
dispersion <anyta1901@gmail.com>
2022-01-18 11:17:51 +0300

    Коммит во второй ветке без решения заданий

--- src/ru/spypro/homework/Main.java
+++ src/ru/spypro/homework/Main.java
@@ -1,5 +1,25 @@
 package ru.spypro.homework;
 
 public class Main {
+    public static void main(String[] args) {
+        exerciseOne();
+        exerciseTwo();
+        exerciseThree();
+        exerciseFour();
+    }
+    public static void exerciseOne() {
 
+    }
+
+    public static void exerciseTwo() {
+
+    }
+
+    public static void exerciseThree() {
+
+    }
+
+    public static void exerciseFour() {
+
+    }
 }
